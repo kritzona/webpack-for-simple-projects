bbf933353733abb844ec63136047ec562754f062
kritzona <virell.m@yandex.ru>
2021-04-11 17:51:26 +0300

    Установил лоадеры для TypeScript и Babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, '/src/index.ts'),
   devtool: 'eval-source-map',
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    open: true,
+  },
   output: {
     path: path.resolve(__dirname, '/dist'),
     publicPath: path.resolve(__dirname, '/public'),
